d5fb5411172c157536b432657714959cd9053679
Maryam Khan <maryam.khan@pcc.edu>
2019-10-03 14:58:13 -0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const io = require("socket.io")();
+io.on("connection", client => {
+  client.on("subscribeToTimer", interval => {
+    console.log("client is subscribing to timer with interval", interval);
+    setInterval(() => {
+      client.emit("timer", new Date().getTime());
+    }, interval);
+  });
+});
+
+const port = 8000;
+io.listen(port);
+console.log("listening on port ", port);
